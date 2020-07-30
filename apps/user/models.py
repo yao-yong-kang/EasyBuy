e89df5b3a88749e455eabe0a6d065aac4a03b267
w1032593545 <1032593545@qq.com>
2020-07-30 10:12:41 +0800

    '拉代码'

--- apps/user/models.py
+++ apps/user/models.py
@@ -6,11 +6,11 @@ import datetime
 # Create your models here.
 class UserProfile(AbstractUser):
     nickName = models.CharField(max_length=25, null=True, verbose_name='昵称')
-    sex = models.IntegerField(default='未设置', verbose_name='性别1为男0为女')
+    sex = models.IntegerField(default=1, verbose_name='性别1为男0为女')
     idcard = models.CharField(max_length=20, default='未设置', verbose_name='身份证')
     email = models.CharField(max_length=30, unique=True, verbose_name='邮箱')
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
-    time = models.DateTimeField(default='', verbose_name='登录时间')
+    time = models.DateTimeField(auto_now_add=True, verbose_name='登录时间')
     score = models.IntegerField(default=0, verbose_name='积分')  # 100scroe=1￥
     money = models.IntegerField(default=0, verbose_name='余额')
     cost = models.IntegerField(default=0, verbose_name='总消费')
--- apps/usercenter/views.py
+++ apps/usercenter/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
+from user.models import UserProfile
+
 
 # Create your views here.
 def memberIndex(request):
-    return render(request, 'usercenter/Member.html')
+    user = UserProfile.objects.get(id=1)
+    print(user.photo)
+    return render(request, 'usercenter/Member.html', {'user': user})
+
+
+def myOrder(request):
+    return render(request, 'usercenter/Member_Order.html')
