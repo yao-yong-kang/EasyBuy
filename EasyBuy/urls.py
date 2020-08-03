a8b6d6e83de07e265dcc622e6911acdc879012a0
w1032593545 <1032593545@qq.com>
2020-08-03 16:47:10 +0800

    '用户中心部分功能'

--- EasyBuy/urls.py
+++ EasyBuy/urls.py
@@ -1,7 +1,6 @@
-
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path,include,re_path
+from django.urls import path, include, re_path
 from django.views.static import serve
 from .settings import MEDIA_ROOT
 from user.views import ActiveUserView
@@ -9,16 +8,15 @@ from goods.views import *
 
 import xadmin
 
-
 urlpatterns = [
 
     path('xadmin/', xadmin.site.urls),
     path('captcha/', include('captcha.urls')),  # 生成验证码图片功能
-    path('user/',include(('user.urls','user'),namespace='user')),  # 用户
-    re_path('active/(?P<active_code>.*)/',ActiveUserView.as_view(),name='active'),  # 邮箱激活的路由
+    path('user/', include('user.urls', namespace='user')),  # 用户
+    re_path('active/(?P<active_code>.*)/', ActiveUserView.as_view(), name='active'),  # 邮箱激活的路由
     path('usercenter/', include('usercenter.urls', namespace='member')),
 
-    url(r'^media/(?P<path>.*)$',  serve, {"document_root": MEDIA_ROOT}),  # 图片路径
-    path('product_detail/',product_detail ,name='product_detail' ),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),  # 图片路径
+    path('product_detail/', product_detail, name='product_detail'),
 
 ]
