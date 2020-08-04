1bf395d462f6f743398c15ca5c5442c28adca701
w1032593545 <1032593545@qq.com>
2020-08-04 10:31:05 +0800

    '2'

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -115,16 +115,15 @@ def product_detail(request):
     id = request.GET.get('id')
     if id:
         product = Product.objects.get(id=id)
+        user = request.user
+        cars = Car.objects.filter(userId=user.id)
         if request.method== "POST":
-            if request.user.is_active:
-
-
-
+            if user:
                 num = request.POST.get('num', 1)
                 Car.objects.create(number= num,productId_id= id,userId_id=request.user.id)
-                return render(request, "goods/ProductDetail.html", {'product': product,'msg':'加入购物车完成'})
+                return redirect(reverse('goodcar1'))
             else:
-                return redirect("/")
-        return render(request,"goods/ProductDetail.html",{'product' : product})
+                return redirect(reverse('user:login'))
+        return render(request,"goods/ProductDetail.html",{'product' : product, 'user':user})
 
-    return redirect("/")
+    return redirect(reverse('goods:index'))
